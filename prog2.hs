fcdef0fb281fec38cf57bca552700f03dad1663f
Guilhermeslucas <guilherme.slucas@gmail.com>
2017-06-04 18:48:49 -0300

    inicialind nodes. Have to erase the ones that appear more than once

--- prog2.hs
+++ prog2.hs
@@ -2,9 +2,17 @@ import Data.List.Split
 
 -- node structure
 data Node = Node { key :: String
-                 , pathCost :: Int
+                 , pathCost :: Float
                  , cameFrom :: String
-}
+} deriving (Show, Eq, Ord)
+
+-- function used to initiate all noes. -1 means infinity
+initNodes paths acc = if paths == [] then acc
+                      else do
+                        let first = (head (head paths))
+                        let second = (head (tail (head paths)))
+                        initNodes (tail paths) acc++[(Node first (- 1) "0")]++[(Node second (- 1) "0")]
+
 
 -- main funtion. will be the first one to be called
 main = do
@@ -14,6 +22,8 @@ main = do
     let begin = inputList!!((length inputList) - 2)
     let destiny = inputList!!((length inputList) - 1)
     let paths = (init (init inputList))
+    let nodes = initNodes paths []
+    print nodes
     print paths
     print begin
     print destiny
